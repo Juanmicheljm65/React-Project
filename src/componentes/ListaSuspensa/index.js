0110c9ca6ae643cf257a327f37c191f477fb0dc3
Juanmicheljm65 <juanmicheljm65@gmail.com>
2024-01-29 17:51:31 -0300

    Finalizando projeto com ajustes finais para resolução de bugs

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -5,7 +5,8 @@ const ListaSuspensa = (props) => {
   return (
     <div className="lista-suspensa">
       <label>{props.label}</label>
-      <select onChange={e => props.aoAlterado(e.target.value)} required={props.obrigatorio} value={props.value}>
+      <select onChange={e => props.aoAlterado(e.target.value)} required={props.obrigatorio} value={props.valor}>
+        <option value=""></option>
         {props.itens.map((item) => {
           return <option key={item}>{item}</option>;
         })}
